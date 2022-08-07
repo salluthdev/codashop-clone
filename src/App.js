0fe1f6dba47aa41d43f2ec03c296ce64b3e3a587
Faisal L <salluthdev@gmail.com>
2022-08-07 18:47:23 +0700

    Pembuatan kerangka list product

--- src/App.js
+++ src/App.js
@@ -5,7 +5,50 @@ function App() {
     <div className="App">
       <main>
         <div className="wrapper">
-          <h1>POPULER</h1>
+          <div className="product-list-container">
+            <h1>POPULER</h1>
+            <div className="product-list">
+              <div className="card">
+                <img src="../images/mobile-legends.jpg" alt="Mobile Legends" />
+              </div>
+              <div className="card">
+                <img src="../images/free-fire.png" alt="Free Fire" />
+              </div>
+              <div className="card">
+                <img src="../images/vidio.jpeg" alt="Vidio" />
+              </div>
+              <div className="card">
+                <img src="../images/valorant.jpg" alt="Valorant" />
+              </div>
+              <div className="card">
+                <img src="../images/genshin-impact.jpg" alt="Genshin Impact" />
+              </div>
+              <div className="card">
+                <img src="../images/league-of-legends-wild-rift.png" alt="League of Legends: Wild Rift" />
+              </div>
+              <div className="card">
+                <img src="../images/ragnarok-x.jpg" alt="Ragnarok X: Next Generation" />
+              </div>
+              <div className="card">
+                <img src="../images/mobile-legends.jpg" alt="Mobile Legends" />
+              </div>
+              <div className="card">
+                <img src="../images/mobile-legends.jpg" alt="Mobile Legends" />
+              </div>
+              <div className="card">
+                <img src="../images/mobile-legends.jpg" alt="Mobile Legends" />
+              </div>
+              <div className="card">
+                <img src="../images/mobile-legends.jpg" alt="Mobile Legends" />
+              </div>
+              <div className="card">
+                <img src="../images/mobile-legends.jpg" alt="Mobile Legends" />
+              </div>
+            </div>
+          </div>
+          <h1>NEW TITLES</h1>
+          <h1>GAMES</h1>
+          <h1>ENTERTAINMENT</h1>
         </div>
       </main>
     </div>
